ae2b60b60b948e36198d344e74fb2e037058331b
Maksym Rohachov <romax982@gmail.com>
2024-01-26 02:53:37 +0100

    Update entry-point

--- src/main/java/org/example/DashboardApplication.java
+++ src/main/java/org/example/DashboardApplication.java
@@ -3,8 +3,10 @@ package org.example;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "org.example.repository")
 public class DashboardApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
